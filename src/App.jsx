d30fa2703d8f120fe3bc2c2af865529181f4a6e0
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-17 20:22:05 +1030

    removal of Auth

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
 // layouts and pages
 import RootLayout from "./layouts/RootLayout.jsx";
 import Home from "./pages/Home/Home.jsx";
-import Auth from "./pages/Auth/Auth.jsx";
+
 
 
 // router and routes
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route path="/" element={<Home />} />
-      <Route path="Auth" element={<Auth />} />
+
     </Route>
   )
 );
